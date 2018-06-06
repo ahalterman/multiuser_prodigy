4468a1e2992162877f801cb0f2203fec9b9674f8
Andy <ahalterman0@gmail.com>
2018-06-06 15:25:22 -0400

    Basic functionality with force restarts; not refreshing every answer

--- multiuser_db.py
+++ multiuser_db.py
@@ -1,19 +1,18 @@
 import prodigy
 from multiprocessing import Process
 from time import sleep
-from prodigy.recipes.ner import batch_train
 import atexit
 from pathlib import Path
-import datetime as dt
 
 from prodigy.components import printers
 from prodigy.components.loaders import get_stream
 from prodigy.core import recipe, recipe_args
 from prodigy.util import TASK_HASH_ATTR, log
 from datetime import datetime
-from collections import Counter
-
-# It's all going to be run by coder name.
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from random import shuffle
 
 # Config:
 # - add list of coders
@@ -22,20 +21,36 @@ from collections import Counter
 # - recipe, db, model, output
 
 class DBStream:
-    def __init__(self, active_coder, db_name):
-        print("Using DB:", db_name)
+    def __init__(self, active_coder, collection_name = "protest_gsr"):
         self.active_coder = active_coder
-        self.db = prodigy.components.db.connect()
-        self.db_name = db_name
+        self.coll = setup_mongo(collection_name)
+        self.reced = 0
+        print("Total tasks in collection: ", self.coll.count())
 
     def get_examples(self):
-        examples = self.db.get_dataset(self.db_name)
-        good = []
-        for eg in examples:
-            if len(eg['coders']) < 3 and self.active_coder not in eg['coders']:
-                good.append(eg)
-        self.examples = iter(good)
+        print("get_examples called")
+        examples = self.coll.find({"$and" : [
+            {"seen" : {"$lt": 3}},
+            {"coders" :  {"$nin" : [self.active_coder]}}]}).limit(200)
+        #examples = self.coll.find().limit(2)
+        #print("Pulling example: ", example)
+        #example = self.coll.find_one()
+        #if example:
+        #    self.coll.update_one({"_id": example['_id']},
+        #                         {"$set": {"reserved": 1}})
+        examples = list(examples)
+        print("inside get_examples, this many examples:", len(examples))
+        for i in examples:
+            i['_id'] = str(i['_id'])
+        shuffle(examples)
+        self.examples = iter(examples)
+        ## !! Need to prioritize examples with 2 or 1 views.
 
+def setup_mongo(collection_name, db_name = "gsr"):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client[db_name]
+    coll = db[collection_name]
+    return coll
 
 @prodigy.recipe('mark_custom',
         dataset=recipe_args['dataset'],
@@ -51,63 +66,73 @@ def mark_custom(dataset, source=None, view_id=None, label='', api=None,
     """
     Click through pre-prepared examples, with no model in the loop.
     """
+
     log('RECIPE: Starting recipe mark', locals())
-    stream = list(get_stream(source, api, loader))
+    coder = source # repurposing input slot
+    stream_empty = iter([])
+    stream = DBStream(coder, "protest_gsr")
+    stream.get_examples()
+    #print("Initial number of examples in queue:", len(list(stream.examples)))
+    #print("Initial examples in queue:", list(stream.examples))
 
-    counts = Counter()
-    memory = {}
-
-    def fill_memory(ctrl):
-        if memorize:
-            examples = ctrl.db.get_dataset(dataset)
-            log("RECIPE: Add {} examples from dataset '{}' to memory"
-                .format(len(examples), dataset))
-            for eg in examples:
-                memory[eg[TASK_HASH_ATTR]] = eg['answer']
-
-    def ask_questions(stream):
-        for eg in stream:
+    def ask_questions(stream_empty):
+        #print("Hitting 'ask_question', with ", len(list(stream.examples)), " in the queue")
+        #print(list(stream.examples))
+        #print(stream.reced)
+        for eg in stream.examples:
+            #stream.get_examples()
             eg['time_loaded'] = datetime.now().isoformat()
-            if TASK_HASH_ATTR in eg and eg[TASK_HASH_ATTR] in memory:
-                answer = memory[eg[TASK_HASH_ATTR]]
-                counts[answer] += 1
-            else:
-                if label:
-                    eg['label'] = label
-                yield eg
+            # not serializeable
+            eg['_id'] = str(eg['_id'])
+            yield eg
+
+
+    #### Problem with the post-answer update.
+    ## Not refreshing
 
     def recv_answers(answers):
         for eg in answers:
-            counts[eg['answer']] += 1
-            memory[eg[TASK_HASH_ATTR]] = eg['answer']
+            print("Answer back: ")#, eg)
+            # Get the example from the DB again in case it's changed
+            updated_ex = list(stream.coll.find({'_id': ObjectId(eg['_id'])}))
+            curr_cod = updated_ex[0]['coders']
+            # add current coder to the list
+            curr_cod.append(coder)
+            stream.coll.update_one({"_id": ObjectId(eg['_id'])}, # convert back
+                                {"$set": {"coders": curr_cod,
+                                          "reserved": 0,
+                                         "seen" : len(curr_cod)}})
             eg['time_returned'] = datetime.now().isoformat()
+            eg['active_coder'] = coder
+            eg['coders'] = curr_cod
+        #print("Refreshing stream...")
+        #stream.get_examples()
 
     def print_results(ctrl):
         print(printers.answers(counts))
 
     def get_progress(session=0, total=0, loss=0):
-        progress = len(counts) / len(stream)
+        progress = 0#len(counts) / len(stream)
         return progress
 
     return {
         'view_id': view_id,
         'dataset': dataset,
-        'stream': ask_questions(stream),
+        'stream': ask_questions(stream_empty),
         'exclude': exclude,
         'update': recv_answers,
-        'on_load': fill_memory,
-        'on_exit': print_results,
-        'config': {'label': label}
+        'on_exit': print_results
+        #'config': {'label': label}
     }
 
 class MultiProdigy:
     def __init__(self,
-        coder_list = [{"name" : "Daniel", "port" : 9010},
+        coder_list = [#{"name" : "Daniel", "port" : 9010},
                        {"name" : "Youseff", "port" : 9011},
-                       {"name" : "Emad", "port" : 9012},
-                       {"name" : "Rafeef", "port" : 9013},
-                       {"name" : "Mahmoud", "port" : 9014},
-                       {"name" : "Zach", "port" : 9015},
+                       #{"name" : "Emad", "port" : 9012},
+                       #{"name" : "Rafeef", "port" : 9013},
+                       #{"name" : "Mahmoud", "port" : 9014},
+                       #{"name" : "Zach", "port" : 9015},
                        {"name" : "Collin", "port" : 9016},
                       ]):
         self.coder_list = coder_list
@@ -115,23 +140,23 @@ class MultiProdigy:
 
     def serve(self, coder, port):
         print(coder)
-        base = "data/protest_for_classification_"
-        filename = "{0}{1}.jsonl".format(base, coder)
+        #base = "data/protest_for_classification_"
+        #filename = "{0}{1}.jsonl".format(base, coder)
         prodigy.serve('mark_custom',       # recipe
-                      "gsr_is_protest",  # db
-                      filename, # input file
+                      "new_test",  # db
+                      coder, # input file, repurposed for coder
                       "classification", # view ID
                       "PROTEST",
                       None, # api
                       None, # loader
                       True, # memorize
-                      "gsr_is_protest", # exclude
+                      "new_test", # exclude
                       port=port)  # port
 
     def make_prodigies(self):
         for coder_info in enumerate(self.coder_list):
             coder_info = coder_info[1] # wut
-            thread =  Process(target=self.serve, args = (coder_info['name'], coder_info['port']))
+            thread = Process(target=self.serve, args = (coder_info['name'], coder_info['port']))
             self.processes.append(thread)
 
     def start_prodigies(self):
@@ -152,13 +177,17 @@ class MultiProdigy:
 
 if __name__ == "__main__":
     mp = MultiProdigy()
-    #mp.make_retrain_time()
     atexit.register(mp.kill_prodigies)
     mp.make_prodigies()
     mp.start_prodigies()
     while True:
-        sleep(5)
-    #    if dt.datetime.now() > mp.retrain_time:
+        sleep(60 * 60)
+        print("Restarting Prodigy...")
+        mp.kill_prodigies()
+        mp.make_prodigies()
+        mp.start_prodigies()
+
+    #    if datetime.datetime.now() > mp.retrain_time:
     #        print("Retraining model and scheduling next retraining for tomorrow")
     #        mp.make_retrain_time() # bump to tomorrow
     #        mp.train_and_restart()
